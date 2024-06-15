98976e108521b7a4becec583626b1c92b9013052
Ayush <ayush3032@gmail.com>
2024-06-16 04:21:33 +0530

    use reducer state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect, createContext, useReducer, useContext } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/screens/Home';
 import Profile from './components/screens/Profile';
@@ -6,21 +7,46 @@ import Signup from './components/screens/Signup';
 import Signin from './components/screens/Signin';
 import CreatePost from './components/screens/CreatePost';
 
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
+import { reducer, initialState } from './reducers/userReducer';
+
+
+export const UserContext = createContext();
+
+const Routing = () => {
+  const navigate = useNavigate();
+  const {state, dispatch} = useContext(UserContext);
+  useEffect(()=> {
+    const user = JSON.parse(localStorage.getItem('user'));
+    dispatch({type: 'USER', payload: user});
+    if(user) {
+      navigate('/');
+    }else{
+      navigate('/signin');
+    }
+  }, []);
+  return (
+    <Routes >
+      <Route exact path="/" element={<Home /> } />
+      <Route path="/signin" element={<Signin /> } />
+      <Route path="/signup" element={<Signup /> } />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/create" element={<CreatePost /> } /> 
+    </Routes >
+  );
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <Router>
-      <Navbar />
-      <Routes >
-        <Route exact path="/" element={<Home /> } />
-        <Route path="/signin" element={<Signin /> } />
-        <Route path="/signup" element={<Signup /> } />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/create" element={<CreatePost /> } /> 
-      </Routes >
-    </Router>
+    <UserContext.Provider value={{state, dispatch}}>
+      <Router>
+        <Navbar />
+        <Routing />
+      </Router>
+    </UserContext.Provider>
 
   );
 }
--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import M from 'materialize-css';
+import { UserContext } from '../../App';
 
 const Signin = () => {
 
+  const { state, dispatch} = useContext(UserContext);
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -20,10 +22,12 @@ const Signin = () => {
       })
     }).then((res => res.json()))
     .then(data => {
-      console.log(data);
       if(data.error) {
         M.toast({html: data.error, classes: '#c62828 red darken-3'});
       }else{
+        localStorage.setItem('jwt', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        dispatch({type: 'USER', payload: data.user})
         M.toast({html: `${data.user.name} ${data.message}`, classes: '#43a047 green darken-1'});
         navigate('/');
 
