8d883c85a20a8f9969cf858287b696694de25ce4
Ayush <ayush3032@gmail.com>
2024-06-16 00:32:43 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/screens/Home';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup';
+import Login from './components/screens/Login';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" > <Home /> </Route>
+      <Route path="/login"> <Login /> </Route>
+      <Route path="/signup"> <Signup /> </Route>
+      <Route path="/profile"> <Profile /> </Route>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+    <div className="nav-wrapper white">
+      <a href="/" className="brand-logo">Instagram</a>
+      <ul id="nav-mobile" className="right">
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">SignUp</a></li>
+        <li><a href="/profile">Profile</a></li>
+      </ul>
+    </div>
+  </nav>
+  );
+}
+
+export default Navbar;
