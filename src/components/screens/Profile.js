2a0f5a4db525e1aeebc96c7d0eb824a83c4049a5
Ayush <ayush3032@gmail.com>
2024-06-16 01:08:47 +0530

    profile page design

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <h1>Profile</h1>
+    <div className='profile'>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        margin: '18px 0px', 
+        borderBottom: '1px solid grey'
+      }} >
+        <div>
+          <img style={{width: "160px", height:"160px", borderRadius: "80px" }} 
+          src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
+          />
+        </div>
+        <div>
+          <h4>Ayush Gupta</h4>
+          <div style={{ display: 'flex', justifyContent: 'space-between', width: '110%'}}>
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+
+      <div className='gallery'>
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img className='item' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+      </div>
+    </div>
   );
 }
 
