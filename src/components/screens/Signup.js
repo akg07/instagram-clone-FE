0b89772786d85bba03425ab92037b81c981a96ca
Ayush <ayush3032@gmail.com>
2024-06-16 02:47:41 +0530

    signup with data

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import M from 'materialize-css';
+import {useHistory} from 'react-router-dom';
 
 const Signup = () => {
+
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const postData = () => {
+    fetch('/signup', {
+      method: 'post', 
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      })
+    }).then(res => res.json())
+    .then(data => {
+      if(data.error) {
+        M.toast({html: data.error, classes: '#c62828 red darken-3'});
+      }else {
+        M.toast({html: data.message, classes: '#43a047 green darken-1'});
+        history.push('/signin');
+      }
+    })
+  }
+
   return (
     <div className="mycard">
       <div className="card auth-card input-field">
       <h2>Instagram</h2>
-      <input type="text" placeholder='name' />
-      <input type="text" placeholder='email' />
-      <input type="text" placeholder='password' />
+      <input type="text" placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="email" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="text" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
-      <button className="btn waves-effect waves-light signin-button blue darken-2">
+      <button className="btn waves-effect waves-light signin-button blue darken-2" 
+          onClick={() => postData()}>
         Signup
       </button>
       <h5> <a href="/signin"> Already have an account? Signin</a> </h5>
