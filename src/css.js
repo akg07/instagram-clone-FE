c741e9d86314ea4084006e4efbd6043be57a9a27
Ayush <ayush3032@gmail.com>
2024-06-24 00:46:33 +0530

    Update my-feed ui

---
+++ src/css.js
@@ -0,0 +1,13 @@
+export const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    height: '50%',
+    width: '40%',
+    position: 'relative',
+  },
+};
